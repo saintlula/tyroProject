f45ce7eca9950e44539e8b2901e23bfbaf666c45
Ehlinaz_YILDIRIM <ehlinazdy@gmail.com>
2026-06-07 15:36:21 +1000

    09 - Configure Vite Proxy and simple global style

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+//Vite configuration file
 export default defineConfig({
   plugins: [react()],
+
+  server:
+  {
+    proxy:
+    {
+      //Any request starting with /api gets forwarded to the backend.
+      //This means the frontend can call /api/orders directly
+      //without needing to know the backend URL or dealing with CORS errors.
+      '/api': 
+      {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
+  }
 })
